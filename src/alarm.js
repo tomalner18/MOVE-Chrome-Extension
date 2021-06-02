ed82b31680be3cb98e8bafbf6de64446ddaae52d
Jordan  Kisa <jordan.kisa18@imperial.ac.uk>
2021-06-02 10:06:29 +0100

    Added notification sound

--- src/alarm.js
+++ src/alarm.js
@@ -4,13 +4,16 @@ chrome.alarms.onAlarm.addListener(function() {
   chrome.browserAction.setBadgeText({text: ''});
   chrome.notifications.create({
       type:     'basic',
-      iconUrl:  'icons/logo128.png',
-      title:    'Time to Hydrate',
-      message:  'Everyday I\'m Guzzlin\'!',
+      iconUrl:  'icons/logo48.png',
+      requireInteraction: true,
+      title:    'Study Break!',
+      message:  'It has come to the end of this study period, please take a break!',
       buttons: [
-        {title: 'Keep it Flowing.'}
+        {title: 'exercises'}
       ],
       priority: 0});
+      var yourSound = new Audio('./alarm.mp3');
+      yourSound.play();
 });
 
 chrome.notifications.onButtonClicked.addListener(function() {
