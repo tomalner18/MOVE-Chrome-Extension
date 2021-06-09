5f2db4e479ec0f1ede22bb6f6f5af2c5202330c4
Tom <tla19@ic.ac.uk>
2021-06-09 17:43:00 +0100

    add tab changing

--- src/popup.js
+++ src/popup.js
@@ -12,18 +12,62 @@ function clearAlarm() {
     window.close();
 }
 
-function openTab(tabName) {
-    var i;
-    var x = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block"
+function clearTabs() {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+}
+
+function openHome() {
+  var x = document.getElementsByClassName("active");
+  x[0].classList.remove("active");
+  clearTabs();
+  document.getElementById("homepage").style.display = "inline";
+  var y = document.getElementById("home-btn");
+  y.classList.add("active")
+}
+
+function openStat() {
+  var x = document.getElementsByClassName("active");
+  x[0].classList.remove("active");
+  clearTabs();
+  document.getElementById("statpage").style.display = "inline";
+  var y = document.getElementById("stat-btn");
+  y.classList.add("active")
+}
+
+function openCalendar() {
+  var x = document.getElementsByClassName("active");
+  x[0].classList.remove("active");
+  clearTabs();
+  document.getElementById("homepage").style.display = "inline";
+  var y = document.getElementById("calendar-btn");
+  y.classList.add("active")
+}
+
+function openSocial() {
+  var x = document.getElementsByClassName("active");
+  x[0].classList.remove("active");
+  clearTabs();
+  document.getElementById("homepage").style.display = "inline";
+  var y = document.getElementById("social-btn");
+  y.classList.add("active")
+}
+
+function iniNav() {
+  document.getElementById('home-btn').addEventListener('click', openHome);
+  document.getElementById('stat-btn').addEventListener('click', openStat);
+  document.getElementById('calendar-btn').addEventListener('click', openHome);
+  document.getElementById('social-btn').addEventListener('click', openHome);
+  document.getElementById('more-btn').addEventListener('click', openHome);
 }
   
 //An Alarm delay of less than the minimum 1 minute will fire
 // in approximately 1 minute incriments if released
-document.getElementById('sampleSecond').addEventListener('click', setAlarm);
-document.getElementById('15min').addEventListener('click', setAlarm);
-document.getElementById('30min').addEventListener('click', setAlarm);
-document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
+  iniNav();
+  document.getElementById('sampleSecond').addEventListener('click', setAlarm);
+  document.getElementById('15min').addEventListener('click', setAlarm);
+  document.getElementById('30min').addEventListener('click', setAlarm);
+  document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
