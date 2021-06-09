352a8b7d46ff8b981d4110f28f2b8849c28ff516
Tom <tla19@ic.ac.uk>
2021-06-09 14:02:27 +0100

    create UI topbar

--- src/popup.js
+++ src/popup.js
@@ -11,6 +11,10 @@ function setAlarm(event) {
     chrome.alarms.clearAll();
     window.close();
   }
+
+  function openTab() {
+    
+  }
   
   //An Alarm delay of less than the minimum 1 minute will fire
   // in approximately 1 minute incriments if released
