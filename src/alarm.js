85aa73db6229e560aac7c0c020dd83959573af13
Tom <tla19@ic.ac.uk>
2021-06-11 11:31:41 +0100

    clean up functions, set-up for countdown timer

--- src/alarm.js
+++ src/alarm.js
@@ -2,6 +2,35 @@
 
 var myNotificationID = null;
 
+var default_config = {
+  study_length: 40,
+  break_length: 5,
+  break_message: "It has come to the end of this study period, please take a break!",
+  snooze_length: 5,
+  idle_reset_enabled: false,
+  idle_reset_minutes: 5,
+  skip_break_enabled: true,
+  snooze_enabled: true
+}
+
+// Creates a Chrome Notification
+function createNotification() {
+  chrome.notifications.create(`move-notification-${Date.now()}`, {
+    type: "basic",
+    iconUrl: "icons/logo48.png",
+    requireInteraction: true,
+    title:    'Study Break!',
+    message:  default_config.break_message,
+    buttons: [
+      {title: 'Exercises'}
+    ],
+    priority: 0}, function(id) {
+      myNotificationID = id;
+    });
+    var yourSound = new Audio('./alarm.mp3');
+    yourSound.play();
+  }
+
 chrome.alarms.onAlarm.addListener(function() {
   chrome.browserAction.setBadgeText({text: ''});
   chrome.notifications.create(`my-notification-${Date.now()}`,{
@@ -16,9 +45,13 @@ chrome.alarms.onAlarm.addListener(function() {
       priority: 0}, function(id) {
         myNotificationID = id;
       });
-      var yourSound = new Audio('./alarm.mp3');
-      yourSound.play();
+      playSound();
 });
+
+function playSound() {
+  var yourSound = new Audio('./alarm.mp3');
+  yourSound.play();
+}
 
 
 
