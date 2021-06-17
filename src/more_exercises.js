c2402457a408fb677632cc27205212cf1b54e88c
kianchong <kianchonglow314@gmail.com>
2021-06-17 04:03:54 +0100

    renamed exercises.html, and added grid system to contain multiple instagram post embeds

---
+++ src/more_exercises.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import instagram_embed from "./instagram_embed";
+
+console.log("from exercises.js");
+
+// generate random integer from 1 to 31, as there are 31 videos stored in the database right now.
+const randomInt = Math.floor(Math.random() * 31) + 1
+const api_query = "https://moveimperial.herokuapp.com/api/videos/" + randomInt;
+
+const blockquote_tag = document.querySelector(".instagram-media");
+const hyperlink_tag = document.querySelector(".instagram-media-2");
+const header = document.querySelector(".video_title");
+
+
+axios.get(api_query)
+.then(function (response) {
+    console.log(response);
+    console.log(response.data.filePath);
+
+    const url = response.data.filePath + "?utm_source=ig_embed&amp;utm_campaign=loading";
+    blockquote_tag.setAttribute("data-instgrm-permalink", url);
+    hyperlink_tag.href = url;
+
+    header.textContent = response.data.title;
+
+    instagram_embed.embed();
+});
+
+
