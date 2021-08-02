aea17e66669b0977f96047ec91a5cee9a388d931
Tom <tla19@ic.ac.uk>
2021-08-02 18:49:20 +0100

    begin settings change

--- src/popup.js
+++ src/popup.js
@@ -193,7 +193,7 @@ function iniNav() {
   document.getElementById('stat-btn').addEventListener('click', openStat);
   document.getElementById('calendar-btn').addEventListener('click', openCalendar);
   // document.getElementById('social-btn').addEventListener('click', openSocial);
-  // document.getElementById('more-btn').addEventListener('click', openMore);
+  document.getElementById('more-btn').addEventListener('click', openMore);
 }
 
 function displayWorkingHomePage() {
